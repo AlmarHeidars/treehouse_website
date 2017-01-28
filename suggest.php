7a055c92e703464c5322de6b6ecd44d121631f59
Almar Heidarsson <almarheidars@gmail.com>
2017-01-28 13:08:16 -0500

    setting up intial forms on suggest page

--- suggest.php
+++ suggest.php
@@ -5,7 +5,13 @@ $section = "suggest";
 include("inc/header.php"); ?>
 
 <div class="section page">
-    <h1>Suggest a Media Item</h1>
+    <div class="wrapper">
+        <h1>Suggest a Media Item</h1>
+        <p>If you think there is something I&rsquo;m missing, let me know! Complete the form to send me and email</p>
+        <form method="post">
+            <label for="name">Name</label><input type="text" name="name" id="name" />
+        </form>
+    </div>
 </div>
 
 <?php include("inc/footer.php"); ?>
