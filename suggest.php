156d418806a435e536c8ef7166699cbc27033854
Almar Heidarsson <almarheidars@gmail.com>
2017-01-28 13:12:49 -0500

    creating input fields on form and submit button

--- suggest.php
+++ suggest.php
@@ -9,7 +9,21 @@ include("inc/header.php"); ?>
         <h1>Suggest a Media Item</h1>
         <p>If you think there is something I&rsquo;m missing, let me know! Complete the form to send me and email</p>
         <form method="post">
-            <label for="name">Name</label><input type="text" name="name" id="name" />
+        <table>
+        <tr>
+            <th><label for="name">Name</label></th>
+            <td><input type="text" name="name" id="name" /></td>
+        </tr>
+        <tr>
+            <th><label for="email">Email</label></th>
+            <td><input type="text" name="email" id="email" /></td>
+        </tr>
+        <tr>
+            <th><label for="name">Suggest Item Details</label></th>
+            <td><textarea name="details" id="details"></textarea></td>
+        </tr>
+        </table>
+        <input type="submit" value="Send" />
         </form>
     </div>
 </div>
